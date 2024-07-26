b45119ab1d4eb873aa88dc18c4ddf787af3df83f
cristianciula <cristianciula@gmail.com>
2024-07-26 09:45:02 +0300

    Updates

--- src/test/java/wrappers/SeleniumWrapper.java
+++ src/test/java/wrappers/SeleniumWrapper.java
@@ -17,76 +17,74 @@ public final class SeleniumWrapper {
 
 
     //____________________PUBLIC METHODS____________________//
-    public static WebElement findElement(By by) {
-        return driver.findElement(by);
+    public static WebElement findElement(By locator) {
+        return driver.findElement(locator);
     }
 
-    public static List<WebElement> findElements(By by) {
-        return driver.findElements(by);
+    public static List<WebElement> findElements(By locator) {
+        return driver.findElements(locator);
     }
 
-    public static void scrollToElement(By by) {
+    public static void scrollToElement(By locator) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView();", findElement(by));
+        js.executeScript("arguments[0].scrollIntoView();", findElement(locator));
     }
 
-    public static void navigateToUrl(String url) {
+    public static void getUrl(String url) {
         driver.get(url);
     }
 
-    public static void clickElement(By by) {
-        scrollToElement(by);
-        driver.findElement(by).click();
+    public static void clickElement(By locator) {
+        scrollToElement(locator);
+        driver.findElement(locator).click();
     }
 
-    public static void clearElement(By by) {
-        scrollToElement(by);
-        driver.findElement(by).clear();
+    public static void clearElement(By locator) {
+        scrollToElement(locator);
+        driver.findElement(locator).clear();
     }
 
     public static String getCurrentUrl() {
         return driver.getCurrentUrl();
     }
 
-    public static String getElementText(By by) {
-        scrollToElement(by);
-        return driver.findElement(by).getText();
+    public static String getElementText(By locator) {
+        scrollToElement(locator);
+        return driver.findElement(locator).getText();
     }
 
-    public static boolean isElementDisplayed(By by) {
-        scrollToElement(by);
-        return driver.findElement(by).isDisplayed();
+    public static boolean isElementDisplayed(By locator) {
+        scrollToElement(locator);
+        return driver.findElement(locator).isDisplayed();
     }
 
-    public static boolean isElementSelected(By by) {
-        scrollToElement(by);
-        return driver.findElement(by).isSelected();
+    public static boolean isElementSelected(By locator) {
+        scrollToElement(locator);
+        return driver.findElement(locator).isSelected();
     }
 
-    public static boolean isElementEnabled(By by) {
-        scrollToElement(by);
-        return driver.findElement(by).isEnabled();
+    public static boolean isElementEnabled(By locator) {
+        scrollToElement(locator);
+        return driver.findElement(locator).isEnabled();
     }
 
-    public static void sendKeysToElement(By by, String string) {
-        scrollToElement(by);
-        driver.findElement(by).sendKeys(string);
+    public static void sendKeysToElement(By locator, String text) {
+        scrollToElement(locator);
+        driver.findElement(locator).sendKeys(text);
     }
 
-    public static void hoverOverElement(By by) {
+    public static void hoverOverElement(By locator) {
         Actions actions = new Actions(driver);
-        scrollToElement(by);
-        actions.moveToElement(driver.findElement(by));
+        scrollToElement(locator);
+        actions.moveToElement(driver.findElement(locator));
         actions.build().perform();
     }
 
-    public static void dragAndDropElement(By by, By target) {
+    public static void dragAndDropElement(By sourceLocator, By targetLocator) {
         Actions actions = new Actions(driver);
-        scrollToElement(by);
-        actions.dragAndDrop((WebElement) by, (WebElement) target);
+        scrollToElement(sourceLocator);
+        actions.dragAndDrop((WebElement) sourceLocator, (WebElement) targetLocator);
         actions.build().perform();
     }
-    //TODO: Create custom waiter method to wait for a specific exact "string" if the value of that webelement is updated later than the element itself is displayed. Can potentially also add wait time as well.
-    // Method can contain following parameters: (expectedText, waitTimeInSeconds)
 
 }
